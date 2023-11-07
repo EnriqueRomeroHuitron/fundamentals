60ea08f79783e0c8ba52fe86a4d9fce454923998
EnriqueR <50213357+EnriqueRomeroHuitron@users.noreply.github.com>
2023-11-06 20:28:16 -0500

    Added create messages using DOM getElementByID, createElement and appendChild

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,10 +1,16 @@
+// Variables Globales
+
 let ataqueJugador
 let ataqueEnemigo
 
+// Iniciar juego
+
 function iniciarJuego() {
+     // Variables
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
+    // Eventos
     let botonFuego = document.getElementById('boton-fuego')
     botonFuego.addEventListener('click', ataqueFuego)
     let botonAgua = document.getElementById('boton-agua')
@@ -13,6 +19,7 @@ function iniciarJuego() {
     botonTierra.addEventListener('click', ataqueTierra)
 }
 
+// Funciones para mascotas
 function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
@@ -37,14 +44,18 @@ function seleccionarMascotaEnemigo() {
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
     if (mascotaAleatoria == 1) {
+        // Hipodoge
         spanMascotaEnemigo.innerHTML = 'Hipodoge'
     } else if (mascotaAleatoria == 2) {
+        // Capipepo
         spanMascotaEnemigo.innerHTML = 'Capipepo'
+        // Ratigueya
     } else {
         spanMascotaEnemigo.innerHTML = 'Ratigueya'
     }
 }
 
+// Funciones para ataques
 function ataqueFuego() {
     ataqueJugador = 'FUEGO'
     ataqueAleatorioEnemigo()
@@ -68,10 +79,21 @@ function ataqueAleatorioEnemigo() {
     } else {
         ataqueEnemigo = 'TIERRA'
     }
+    crearMensaje()
+}
+
+// Funciones para mensajes
+function crearMensaje(){
+    let sectionMensajes = document.getElementById('mensajes')
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu mascota ataco con ' + ataqueJugador + ', la mascota del enemigo ataco con ' + ataqueEnemigo + ' Pendiente GANASTE 🎉'
+    sectionMensajes.appendChild(parrafo)
 }
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+// NOTA: esta es otra manera de llamar al script despues de que se cargue todo el HTML. La funcion iniciarJuego se carga cuando ya todo el contenido esta cargado.
 
 window.addEventListener('load', iniciarJuego)
